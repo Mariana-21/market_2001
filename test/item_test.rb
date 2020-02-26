39a472cba76aca1103a67214f2bfb02af38c9091
Mariana Cid <cidmariana5@gmail.com>
2020-02-26 10:07:34 -0700

    Method and test created to test item exists.

---
+++ test/item_test.rb
@@ -0,0 +1,12 @@
+require './lib/item'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists
+    item = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_instance_of Item, item
+  end
+end
