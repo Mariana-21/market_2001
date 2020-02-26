6e0d41be3a2459558e951966e6b0d06632e30249
Mariana Cid <cidmariana5@gmail.com>
2020-02-26 10:13:40 -0700

    Test and method created to get attributes of item.

--- test/item_test.rb
+++ test/item_test.rb
@@ -9,4 +9,11 @@ class ItemTest < Minitest::Test
 
     assert_instance_of Item, item
   end
+
+  def test_it_had_attributes
+    item = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_equal 'Peach', item.name
+    assert_equal 0.75, item.price
+  end
 end
